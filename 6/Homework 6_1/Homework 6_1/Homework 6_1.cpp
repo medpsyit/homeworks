0f9e64c1dcbbc730411b819747f180a8b839c6dc
Diana <gggMissDiggg@mail.ru>
2024-10-07 13:24:43 +0300

    added 6th hw

---
+++ 6/Homework 6_1/Homework 6_1/Homework 6_1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#define MODE 1
+
+#ifndef MODE
+#error Необходимо указать константу MODE!
+#endif
+
+int main()
+{
+    setlocale(LC_CTYPE, "rus");
+#if MODE == 0
+    std::cout << "Работаю в режиме тренировки." << std::endl;
+#elif MODE == 1
+    int add(int first, int second);
+    std::cout << "Работаю в боевом режиме." << std::endl;
+    int first{}, second{};
+    std::cout << "Ведите первое число: ";
+    std::cin >> first;
+    std::cout << "Ведите второе число: ";
+    std::cin >> second;
+    std::cout << "Результат сложения: " << add(first, second) << std::endl;
+#else
+    std::cout << "Неизвестный режим. Завершение работы." << std::endl;
+#endif
+    return 0;
+}
+
+int add(int first, int second) { return first + second; }
